f84dd0252956cfae24c67251bf5781b6183e1c69
Srilatha Maddineni <srilatha.maddineni17@gmail.com>
2024-03-22 15:14:33 -0400

    changes in js file

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const weatherIcon=document.querySelector(".weather-icon");
 async function checkWeather(city){
     const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data=await response.json(); 
-    console.log(data);
+    if(response.status==404)
+    {
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "°C";
     document.querySelector(".humidity").innerHTML=data.main.humidity;
@@ -32,6 +37,8 @@ async function checkWeather(city){
         weatherIcon.src="images/mist.png";
     }
     document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+}
 
 }
 searchBtn.addEventListener("click",()=>{
